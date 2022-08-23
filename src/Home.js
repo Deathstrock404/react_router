259c5ad062a25d1513ae4c1786623833c4148a7d
Deathstrock404 <kamaljot216@gmail.com>
2022-08-23 13:39:19 +0530

    this is a project to learn the concepts of react router

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Header from "./component/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <h1>Home Page</h1>
+    </>
+  );
+};
+
+export default Home;
